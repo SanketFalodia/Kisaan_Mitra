bcdb93e5e469e342f522d910da81068e2588b112
Unskilled_here <subodhk1700@gmail.com>
2025-12-31 23:58:27 +0530

    Delete frontend directory

--- frontend/form_generator.py
+++
@@ -1,8 +0,0 @@
-def generate_form(required_fields):
-    return [
-        {
-            "field_name": field,
-            "question": f"{field.replace('_', ' ').title()} बताइए"
-        }
-        for field in required_fields
-    ]
